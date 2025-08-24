baf15e4dc317c65ab3788ba38dec35cc28ded38e
lunagariyaharsh-max <lunagariyaharsh@gmail.com>
2025-08-24 20:19:42 +1000

    Update axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.106.242.235:5001', // live
+  baseURL: 'http://localhost:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
